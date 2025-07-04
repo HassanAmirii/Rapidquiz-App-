62e78423f035d9d821545e1cfc9abe7df6a08072
Hassan Amiri <hassan.amiri@example.com>
2025-07-04 21:18:41 +0100

    Add quiz.html and quiz.js files; implement quiz questions in quiz.json

---
+++ scripts/quiz.js
@@ -0,0 +1,17 @@
+// TODO: goal dynamically display questions on the app
+
+// Load the selected topics from localStorage
+
+// Fetch related questions
+
+// Shuffle and display with options
+
+// Include a timer (e.g. 10–20 mins)
+
+// Submit button → process result
+
+document.addEventListener("DOMContentLoaded", () => {
+  const selectedClass = localStorage.getItem("selectedClass");
+  const selectedSubject = localStorage.getItem("selectedSubject");
+  const selectedTopic = localStorage.getItem("selectedTopic");
+});
